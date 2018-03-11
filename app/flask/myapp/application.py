ed15877314a26547fa7ac744038889cfaab5df38
cc <cc@lyqs-MacBook-Pro.local>
2018-03-10 22:44:30 -0600

    sidestepped Amazon EB permission issue

--- app/flask/myapp/application.py
+++ app/flask/myapp/application.py
@@ -5,66 +5,66 @@ Deployed to Amazon Web Services using Elastic Beanstalk and RDS
 '''
 from flask import Flask, render_template, request
 from application import db
-from application.visual import plot
-from application.prediction import predict_price
+# from application.visual import plot
+# from application.prediction import predict_price
 
 # Pandas
-import pandas as pd
-import numpy as np
-# from flask_bootstrap import Bootstrap
+#import pandas as pd
+#import numpy as np
+from flask_bootstrap import Bootstrap
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
-#Bootstrap(application)
+Bootstrap(application)
 application.debug=True
 # change this to your own value
 application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 # application.secret_key = 'yfvjkI7803otyFhkmgvv'
 
-try:
-    all_parameters = pd.read_sql('SELECT * FROM all_parameters', db.engine)
-    all_means = pd.read_sql('SELECT * FROM all_means', db.engine)
-    # print("all_parameters:")
-    # print(all_parameters)
-    # print("all_means:")
-    # print(all_means)
-    print("Modeling data loaded from SQL successfully.")
-except:
-    print("Failed to retrieve modeling data from SQL.")
+# try:
+#     all_parameters = pd.read_sql('SELECT * FROM all_parameters', db.engine)
+#     all_means = pd.read_sql('SELECT * FROM all_means', db.engine)
+#     # print("all_parameters:")
+#     # print(all_parameters)
+#     # print("all_means:")
+#     # print(all_means)
+#     print("Modeling data loaded from SQL successfully.")
+# except:
+#     print("Failed to retrieve modeling data from SQL.")
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@application.route('/visualization', methods=['GET', 'POST'])
-def visualization():
-    desc = "The historical data of the region is displayed below."
-    if request.method == 'POST':
-        input_zip = request.form['zipcode']
-        if (plot(input_zip)=="error"):
-            [x,y1,y2,miss] = plot("60201")
-            desc = "The input zip code is invalid."
-        else:
-            [x,y1,y2,miss] = plot(input_zip)
-            #location = detect_loc(input_zip)         
-    else:
-        [x,y1,y2,miss] = plot("60201")
-        #location = detect_loc("60201")
-    return render_template('visualization.html', labels = x, data = y1, data2 = y2, data3 = miss, result_desc = desc)
+# @application.route('/visualization', methods=['GET', 'POST'])
+# def visualization():
+#     desc = "The historical data of the region is displayed below."
+#     if request.method == 'POST':
+#         input_zip = request.form['zipcode']
+#         if (plot(input_zip)=="error"):
+#             [x,y1,y2,miss] = plot("60201")
+#             desc = "The input zip code is invalid."
+#         else:
+#             [x,y1,y2,miss] = plot(input_zip)
+#             #location = detect_loc(input_zip)         
+#     else:
+#         [x,y1,y2,miss] = plot("60201")
+#         #location = detect_loc("60201")
+#     return render_template('visualization.html', labels = x, data = y1, data2 = y2, data3 = miss, result_desc = desc)
 
-@application.route('/prediction', methods=['GET', 'POST'])
-def prediction():
-    if request.method == 'POST':
-        # ??? please know how to test the validity of those things!
-        zipcode = request.form['zipcode']
-        # YYYY-MM, e.g. 2018-10
-        date = request.form['date']
-        # e.g. bed2
-        hometype = request.form['hometype']
-        price = predict_price(zipcode, date, hometype, all_parameters, all_means)
-        print(price)
-    return render_template('prediction.html')
+# @application.route('/prediction', methods=['GET', 'POST'])
+# def prediction():
+#     if request.method == 'POST':
+#         # ??? please know how to test the validity of those things!
+#         zipcode = request.form['zipcode']
+#         # YYYY-MM, e.g. 2018-10
+#         date = request.form['date']
+#         # e.g. bed2
+#         hometype = request.form['hometype']
+#         price = predict_price(zipcode, date, hometype, all_parameters, all_means)
+#         print(price)
+#     return render_template('prediction.html')
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
