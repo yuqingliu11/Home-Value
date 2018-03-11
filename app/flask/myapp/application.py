c152a0d215b9abfce4aec83b791a326dfcfea011
cc <cc@lyqs-MacBook-Pro.local>
2018-03-10 19:14:58 -0600

    added application secret key

--- app/flask/myapp/application.py
+++ app/flask/myapp/application.py
@@ -18,7 +18,7 @@ application = Flask(__name__)
 #Bootstrap(application)
 application.debug=True
 # change this to your own value
-# application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
+application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 # application.secret_key = 'yfvjkI7803otyFhkmgvv'
 
 try:
